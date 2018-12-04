17be63a1f9ba064c8fa8e7ec9326baea36091afc
prafful <prraful@gmail.com>
2018-12-04 15:32:06 +0530

    day 2 bangalore SPA routes!

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GrandParentComponent } from './family/grand-parent/grand-parent.component';
+import { TaskManagerComponent } from './mini-app/task-manager/task-manager.component';
 
-const routes: Routes = [];
+
+const myroutes: Routes = [
+           
+            {
+              path:'family',
+              component:GrandParentComponent
+            },
+            {
+              path:'task',
+              component:TaskManagerComponent
+            }
+                      ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(myroutes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
 
 
-import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { GrandParentComponent } from './family/grand-parent/grand-parent.component';
 import { FatherComponent } from './family/father/father.component';
 import { MotherComponent } from './family/mother/mother.component';
 import { TaskManagerComponent } from './mini-app/task-manager/task-manager.component';
+import { NotFoundComponent } from './not-found/not-found/not-found.component';
 
 
 @NgModule({
@@ -17,13 +19,47 @@ import { TaskManagerComponent } from './mini-app/task-manager/task-manager.compo
     GrandParentComponent,
     FatherComponent,
     MotherComponent,
-    TaskManagerComponent
+    TaskManagerComponent,
+    NotFoundComponent
    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+                      {
+                        path:'',
+                        redirectTo:'task',
+                        pathMatch:'full'
+                      },
+                      {
+                        path:'family',
+                        component:GrandParentComponent,
+                        children:[
+                          {
+                            path:'',
+                            redirectTo:'father',
+                            pathMatch:'full'
+                          },
+                          {
+                            path:'father',
+                            component:FatherComponent
+                          },
+                          {
+                            path:'mother',
+                            component:MotherComponent
+                          }
+                        ]
+                      },
+                      {
+                        path:'task',
+                        component:TaskManagerComponent
+                      },
+                      {
+                        path:'**',
+                        component:NotFoundComponent
+                      }
+                    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
